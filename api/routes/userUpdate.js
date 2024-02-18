1c0682494290d302ba879fed1655780ed18d1416
Roberto Nunes <jrvn@cin.ufpe.br>
2024-02-18 12:32:33 -0300

    erro ao recuperar todos os usuarios ao atualizar pagina

--- api/routes/userUpdate.js
+++ api/routes/userUpdate.js
@@ -3,16 +3,18 @@ const router = Router();
 const User = require('../models/User');
 
 router.patch('/user/:id', async (req, res) => {
-        const id = req.params.id;
-        const {name, email, password, role} = req.body;
-        const user = {name, email, password, role};
+    const id = req.params.id;
         try {
+            const {name, email, role} = req.body;
+            const user = {name, email, role};
             const updatedUser = await User.updateOne({_id: id}, user);
             if (updatedUser.matchedCount === 0) {
                 res.status(404).json({error: 'User not found'});
                 return;
             }
-            res.status(200).json({message: 'User updated successfully'});
+            let users = await User.find();
+            console.log(users);
+            res.status(200).json({message: 'User updated successfully', data:users});
         } catch (err) {
             console.error(err);
             res.status(500).json({error: 'An error occurred while updating the user'});
