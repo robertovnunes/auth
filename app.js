dc439a16403a7c2ec3eab5c91102bcd41d140cff
Roberto Nunes <jrvn@cin.ufpe.br>
2024-02-19 16:28:18 -0300

    adicionando icone na aplicação

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const routeDelete = require('./api/routes/userDelete');
 const express = require("express");
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public/views'));
+app.set('views', path.join(__dirname, 'public'));
 app.use(express.static('public'));
 
 // Rota principal
